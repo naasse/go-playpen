76fe245aedb8f4903e5bbae92e041a7d36ba6d28
Nathan Asselstine <nate.asselstine@gmail.com>
2020-08-14 20:43:48 -0400

    feat: setup script, ignoring child projects

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Clone the go-playpen microservices project
+git clone git@github.com:naasse/articles-representations.git
+git clone git@github.com:naasse/articles-service.git
+git clone git@github.com:naasse/rest-representations.git
+
